e149e0968b34c0f95f36fb6c24dc9a712a251924
domenico.ventura <domenico.ventura@deltatre.com>
2017-12-01 16:42:45 +0100

    introduced private methods

--- string-calculators-kata/string_calculator.py
+++ string-calculators-kata/string_calculator.py
@@ -2,15 +2,15 @@ import re
 
 class StringCalculator:
     def add(self, numbers):
-        delimiters = self.retrieveDelimiters(numbers)
+        delimiters = self.__retrieveDelimiters(numbers)
         numList = re.split(delimiters, numbers)
         total = 0
         for val in numList:
-            total = total  + self.str_to_int(val)
+            total = total  + self.__str_to_int(val)
         
         return int(total)
 
-    def str_to_int(self, str):
+    def __str_to_int(self, str):
         value=0
         try: 
             value= int(str)
@@ -20,7 +20,7 @@ class StringCalculator:
             raise NegativeNotAllowedException('negatives not allowed: {0}'.format(value))
         return value if value<=1000 else 0
 
-    def retrieveDelimiters(self, str):
+    def __retrieveDelimiters(self, str):
         if (str.startswith("//")) and (str.index("\n") >2):
            return re.compile(str[2:str.index('\n')] )
         else:
