bd8bbe624bc2094b2c665a52178416f4b4843c6c
domenico.ventura <domenico.ventura@deltatre.com>
2017-12-01 15:20:32 +0100

    moving from static methods to object instances

--- string-calculators-kata/string_calculator.py
+++ string-calculators-kata/string_calculator.py
@@ -1,18 +1,16 @@
 import re
 
 class StringCalculator:
-    @staticmethod
-    def add(numbers):
-        delimiters = StringCalculator.retrieveDelimiters(numbers)
+    def add(self, numbers):
+        delimiters = self.retrieveDelimiters(numbers)
         numList = re.split(delimiters, numbers)
         total = 0
         for val in numList:
-            total = total  + StringCalculator.str_to_int(val)
+            total = total  + self.str_to_int(val)
         
         return int(total)
 
-    @staticmethod
-    def str_to_int(str):
+    def str_to_int(self, str):
         value=0
         try: 
             value= int(str)
@@ -21,10 +19,8 @@ class StringCalculator:
         if value<0:
             raise NegativeNotAllowedException('negatives not allowed: {0}'.format(value))
         return value if value<=1000 else 0
-            
 
-    @staticmethod
-    def retrieveDelimiters(str):
+    def retrieveDelimiters(self, str):
         if (str.startswith("//")) and (str.index("\n") >2):
            return re.compile(str[2:str.index('\n')] )
         else:
--- string-calculators-kata/string_calculator_tests.py
+++ string-calculators-kata/string_calculator_tests.py
@@ -4,64 +4,67 @@ import unittest
 
 
 class StringCalculatorsTest(unittest.TestCase):
+    def setUp(self):
+        self.calculator= StringCalculator()
+    
     def test_stringcalculator_emptylist_shouldreturn0(self):
-        self.assertEqual(0,StringCalculator.add(""))
+        self.assertEqual(0,self.calculator.add(""))
 
     def test_stringcalculator_listof1_shouldreturn1(self):
-        self.assertEqual(1,StringCalculator.add("1"))
+        self.assertEqual(1,self.calculator.add("1"))
 
     def test_stringcalculator_listofN_shouldreturnN(self):
         rand = randint(1,1000)
-        self.assertEqual(1,StringCalculator.add("1"))
-        self.assertEqual(7,StringCalculator.add("7"))
-        self.assertEqual(99,StringCalculator.add("99"))
-        self.assertEqual(0,StringCalculator.add("0"))
-        self.assertEqual(rand,StringCalculator.add(str(rand)))
+        self.assertEqual(1,self.calculator.add("1"))
+        self.assertEqual(7,self.calculator.add("7"))
+        self.assertEqual(99,self.calculator.add("99"))
+        self.assertEqual(0,self.calculator.add("0"))
+        self.assertEqual(rand,self.calculator.add(str(rand)))
 
     def test_stringcalculator_listoftwo_shouldreturnsumoftwo(self):
-        self.assertEqual(StringCalculator.add("1,2"),3)
-        self.assertEqual(StringCalculator.add("5,10"),15)
-        self.assertEqual(StringCalculator.add("88,9"),88+9)
+        self.assertEqual(self.calculator.add("1,2"),3)
+        self.assertEqual(self.calculator.add("5,10"),15)
+        self.assertEqual(self.calculator.add("88,9"),88+9)
         rand1 = randint(1,1000)
         rand2 = randint(1,1000)
-        self.assertEqual(StringCalculator.add('{0!s},{1!s}'.format(rand1,rand2)),rand1+rand2)
+        self.assertEqual(self.calculator.add('{0!s},{1!s}'.format(rand1,rand2)),rand1+rand2)
     
     def test_stringcalculator_listendingwithcomma_shouldnotbreak(self):
-        self.assertEqual(StringCalculator.add("1,"),1)
+        self.assertEqual(self.calculator.add("1,"),1)
 
     def test_stringcalculator_liststartingwithcomma_shouldnotbreak(self):
-        self.assertEqual(StringCalculator.add(",8"),8)
+        self.assertEqual(self.calculator.add(",8"),8)
 
     def test_stringcalculator_listwithmultipleemptycommas_shouldnotbreak(self):
-        self.assertEqual(StringCalculator.add(",,,,8,,,,"),8)
-        self.assertEqual(StringCalculator.add(",,3,,,,5,,"),8)
-        self.assertEqual(StringCalculator.add("3,,,,,,5,,"),8)
-        self.assertEqual(StringCalculator.add(",,3,,,,,,5"),8)
-        self.assertEqual(StringCalculator.add("3,,,,,,,,5"),8)
+        self.assertEqual(self.calculator.add(",,,,8,,,,"),8)
+        self.assertEqual(self.calculator.add(",,3,,,,5,,"),8)
+        self.assertEqual(self.calculator.add("3,,,,,,5,,"),8)
+        self.assertEqual(self.calculator.add(",,3,,,,,,5"),8)
+        self.assertEqual(self.calculator.add("3,,,,,,,,5"),8)
 
     def test_stringcalculator_multiplenumber_shouldreturnsum(self):
-        self.assertEqual(StringCalculator.add("1,2,3,4"),10)
-        self.assertEqual(StringCalculator.add("2,2,2,2,2,2,2,2,2,2"),20)
+        self.assertEqual(self.calculator.add("1,2,3,4"),10)
+        self.assertEqual(self.calculator.add("2,2,2,2,2,2,2,2,2,2"),20)
 
     def test_stringcalculator_newlinesarelikecommas_shouldreturnsum(self):
-        self.assertEqual(StringCalculator.add("1\n2,3"),6)
-        self.assertEqual(StringCalculator.add("1\n,,"),1)
+        self.assertEqual(self.calculator.add("1\n2,3"),6)
+        self.assertEqual(self.calculator.add("1\n,,"),1)
 
     def test_stringcalculator_changedelimiterintheformat_shouldreturnsum(self):
-        self.assertEqual(StringCalculator.add("//;\n1;2"),3)
-        self.assertEqual(StringCalculator.add("//kk\n1kk2"),3)
-        self.assertEqual(StringCalculator.add("///\n1/2"),3)
+        self.assertEqual(self.calculator.add("//;\n1;2"),3)
+        self.assertEqual(self.calculator.add("//kk\n1kk2"),3)
+        self.assertEqual(self.calculator.add("///\n1/2"),3)
 
     def test_stringcalculator_passingnegativenumber_shouldthrowexception(self):
         with self.assertRaises(NegativeNotAllowedException) as ctx:
-            StringCalculator.add("1,-2")
+            self.calculator.add("1,-2")
         exc = ctx.exception
         self.assertEqual("negatives not allowed: -2",str(exc))    
 
 
     def test_stringcalculator_numberbiggerthan1000_shouldbeignore(self):
-        self.assertEqual(StringCalculator.add("1001,1"),1)
-        self.assertEqual(StringCalculator.add("1000,1"),1001)
+        self.assertEqual(self.calculator.add("1001,1"),1)
+        self.assertEqual(self.calculator.add("1000,1"),1001)
     
 
 
