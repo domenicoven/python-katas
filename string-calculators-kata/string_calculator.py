46fb9a4d3f57668c7303b8a5e71287fecdf6767d
domenico.ventura <domenico.ventura@deltatre.com>
2017-12-01 13:30:34 +0100

    two numbers

--- string-calculators-kata/string_calculator.py
+++ string-calculators-kata/string_calculator.py
@@ -1,4 +1,10 @@
+from functools import reduce
+
 class StringCalculator:
     @staticmethod
     def add(numbers):
+        if(len(numbers) >0):
+            numList = numbers.split(",")
+            total = reduce(lambda curr, tot: int(tot) + int(curr), numList)
+            return int(total)
         return 0
--- string-calculators-kata/string_calculator_tests.py
+++ string-calculators-kata/string_calculator_tests.py
@@ -1,8 +1,29 @@
 from string_calculator import StringCalculator
+from random import *
 import unittest
 
 
 class StringCalculatorsTest(unittest.TestCase):
     def test_stringcalculator_emptylist_shouldreturn0(self):
-        
         self.assertEqual(0,StringCalculator.add(""))
+
+    def test_stringcalculator_listof1_shouldreturn1(self):
+        self.assertEqual(1,StringCalculator.add("1"))
+
+    def test_stringcalculator_listofN_shouldreturnN(self):
+        rand = randint(1,999999)
+        self.assertEqual(1,StringCalculator.add("1"))
+        self.assertEqual(7,StringCalculator.add("7"))
+        self.assertEqual(1234567,StringCalculator.add("1234567"))
+        self.assertEqual(0,StringCalculator.add("0"))
+        self.assertEqual(rand,StringCalculator.add(str(rand)))
+
+    def test_stringcalculator_listoftwo_shouldreturnsumoftwo(self):
+        self.assertEqual(StringCalculator.add("1,2"),3)
+        self.assertEqual(StringCalculator.add("5,10"),15)
+        self.assertEqual(StringCalculator.add("88,9"),88+9)
+        rand1 = randint(1,999999)
+        rand2 = randint(1,999999)
+        self.assertEqual(StringCalculator.add('{0!s},{1!s}'.format(rand1,rand2)),rand1+rand2)
+
+        
